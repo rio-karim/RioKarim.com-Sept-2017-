993edd5802c933f3ebbb51b2cbf88d09cd3fe723
Rio Karim <rio.karim@gmail.com>
2017-09-20 14:30:44 +0100

    #18 much cleaner JQ

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,76 +1,65 @@
 $(function() {
 
-  /* TO DO
+  /*
+  PAGE FADE FUNCTIONS
+  */
 
-    CLEANUP TIMEOUTS. PASS ID AND TIMEOUT AS ARGUMENTS IN A FUNCTION
-    CLEANUP OPACITY 0 - 1 WITH ARGUMENT / FUNCTION
-
-    */
-
+  var fadeID = ["#loadingLine", "#scrollArrow", "#personalBar", "#contactBar", ".content", "#projectsHeader"];
+  var fadeNUM = [200, 1000, 1000, 1000, 1000, 1000, 1000];
+  var timeoutNUM = [0, 6000, 4240, 6000, 6200, 6200];
   $('#myName').fadeOut(1).css({'opacity': 1});
-  $('#personalBar').fadeOut(1).css({'visibility': 'visible'});
-  $('#contactBar').fadeOut(1).css({'visibility': 'visible'});
-  $('#scrollArrow').fadeOut(1).css({'visibility': 'visible'});
-  $('.content').fadeOut(1).css({'visibility': 'visible'});
-  $('#projectsHeader').fadeOut(1).css({'visibility': 'visible'});
-  $('#loadingLine').fadeOut(1).css({'visibility': 'visible'});
-    $('#loadingLine').fadeIn(300);
+
+  function setVisible(stringID){
+    $(stringID).fadeOut(1).css({'visibility': 'visible'});
+  };
+
+  function timeoutFadeIn(stringID, intFade, intTimeout){
+    setTimeout(function () {
+        $(stringID).fadeIn(intFade);
+    }, intTimeout);
+  };
+
+
+  for (var i = 0; i < fadeID.length; i++){
+    setVisible(fadeID[i]);
+    timeoutFadeIn(fadeID[i], fadeNUM[i], timeoutNUM[i]);
+  };
+
+/*
+SKILLBAR AND NAME FADE (SEPERATE FOR LOADING PURPOSES)
+*/
 
   setTimeout(		function (){
     for (var i = 1; i < 9; i++){
       if (i == 1) {
         $('#skillsBar').css({'opacity': 1});
-          $('#myName').fadeIn(3000);
+        $('#myName').fadeIn(3000);
       }
-      $('#skill'+i).fadeOut(1)
+      $('#skill'+i).fadeOut(1);
       $('#skill'+i).fadeIn((i*1000)/2);
-
-    }
+    };
   }, 3000);
 
-  setTimeout(function () {
-      $("#loadingLine").fadeOut(200);
-  }, 1000);
+/*
+SCROLL ARROW FADE ON SCROLL
+*/
 
-
-  setTimeout(function () {
-      $("#personalBar").fadeIn(1000);
-  }, 4240);
-
-  setTimeout(function () {
-      $("#scrollArrow").fadeIn(1000);
-      $("#contactBar").fadeIn(1000);
-  }, 6000);
-
-  setTimeout(function () {
-    $(".content").fadeIn(1000);
-    $("#projectsHeader").fadeIn(1000);
-  }, 6200);
+  $(window).scroll(function (){
+    if ($(this).scrollTop()>0){
+        $('#scrollArrow').fadeOut();
+     }
+    else{
+      $('#scrollArrow').fadeIn();
+    }
+  });
 
 
 /*
-  setTimeout(function () {for (var i = 0; i < 2; i++){
-    $('#scrollArrow').animate({
-      bottom: "8"
-    }, 500).animate({
-      bottom: "5"
-    }, 500);
-      }
-  }, 6500);
-  */
-
-  $(window).scroll(function() {
-
-  if ($(this).scrollTop()>0)
-   {
-      $('#scrollArrow').fadeOut();
-   }
-  else
-   {
-    $('#scrollArrow').fadeIn();
-   }
-});
-
+LOADING ANIMATION
+*/
+setTimeout(function () {
+    $("#loadingLine").fadeOut(100);
+}, 950);
 
   $(function () {
       $('#loadingLine').animate({
@@ -85,10 +74,27 @@ $(function() {
             left : "100"
         }, 500);
     });
+});
+
+
+/* REDUNDANT SCRIPT */
+
+/* ARROW ANIMATE
+
+
+  setTimeout(function () {for (var i = 0; i < 2; i++){
+    $('#scrollArrow').animate({
+      bottom: "8"
+    }, 500).animate({
+      bottom: "5"
+    }, 500);
+      }
+  }, 6500);
+  */
 
 
 /* SCROLL LOCK */
-
+/*
 function lockScroll() {
 $('html, body').css({
   overflow: 'hidden',
@@ -104,7 +110,7 @@ $('html, body').css({
 }
 
 
-/*
+
 lockScroll();
 setTimeout(unlockScroll, 10000);
 
@@ -114,7 +120,6 @@ setTimeout(function (){
   if($(window).width() <= 463){
     $('body').css({'background-size': '150%'});
   }
-}, 4200);*/
+}, 4200);
 
-
-});
+*/
