9e7f8480e73f24f5c2742887e0f176a57a838a6c
Rio Karim <rio.karim@gmail.com>
2017-09-21 18:43:58 +0100

    CSS branch clean and beautiful

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -4,77 +4,77 @@ $(function() {
   PAGE FADE FUNCTIONS
   */
 
-  var fadeID = ["#loadingLine", "#scrollArrow", "#personalBar", "#contactBar", ".content", "#projectsHeader"];
-  var fadeNUM = [200, 1000, 1000, 1000, 1000, 1000, 1000];
-  var timeoutNUM = [0, 6000, 4240, 6000, 6200, 6200];
-  $('#myName').fadeOut(1).css({'opacity': 1});
+  var fadeID = ['.loadingLine', '.scrollArrow', '.personalBar', '.contactBar', '.content', '.projectsHeader']
+  var fadeNUM = [200, 1000, 1000, 1000, 1000, 1000, 1000]
+  var timeoutNUM = [0, 6000, 4240, 6000, 6200, 6200]
+  $('.myName').fadeOut(1).css({'opacity': 1})
 
-  function setVisible(stringID){
-    $(stringID).fadeOut(1).css({'visibility': 'visible'});
+  function setVisible(stringID) {
+    $(stringID).fadeOut(1).css({'visibility': 'visible'})
   };
 
-  function timeoutFadeIn(stringID, intFade, intTimeout){
+  function timeoutFadeIn(stringID, intFade, intTimeout) {
     setTimeout(function () {
-        $(stringID).fadeIn(intFade);
-    }, intTimeout);
+        $(stringID).fadeIn(intFade)
+    }, intTimeout)
   };
 
 
-  for (var i = 0; i < fadeID.length; i++){
-    setVisible(fadeID[i]);
-    timeoutFadeIn(fadeID[i], fadeNUM[i], timeoutNUM[i]);
+  for (var i = 0; i < fadeID.length; i++) {
+    setVisible(fadeID[i])
+    timeoutFadeIn(fadeID[i], fadeNUM[i], timeoutNUM[i])
   };
 
 /*
 SKILLBAR AND NAME FADE (SEPERATE FOR LOADING PURPOSES)
 */
 
-  setTimeout(		function (){
-    for (var i = 1; i < 9; i++){
-      if (i == 1) {
-        $('#skillsBar').css({'opacity': 1});
-        $('#myName').fadeIn(3000);
+  setTimeout(function () {
+    for (var i = 1; i < 9; i++) {
+      if (i === 1) {
+        $('.skillsBar').css({'opacity': 1})
+        $('.myName').fadeIn(3000);
       }
-      $('#skill'+i).fadeOut(1);
-      $('#skill'+i).fadeIn((i*1000)/2);
+      $('#skill' + i).fadeOut(1)
+      $('#skill' + i).fadeIn((i * 1000) / 2)
     };
-  }, 3000);
+  }, 3000)
 
 /*
 SCROLL ARROW FADE ON SCROLL
 */
 
-  $(window).scroll(function (){
-    if ($(this).scrollTop()>0){
-        $('#scrollArrow').fadeOut();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('.scrollArrow').fadeOut()
      }
-    else{
-      $('#scrollArrow').fadeIn();
+     else{
+      $('.scrollArrow').fadeIn()
     }
-  });
+  })
 
 
 /*
 LOADING ANIMATION
 */
-setTimeout(function () {
-    $("#loadingLine").fadeOut(100);
-}, 950);
+  setTimeout(function () {
+    $('.loadingLine').fadeOut(100)
+  }, 950)
 
   $(function () {
-      $('#loadingLine').animate({
-            left : "100"
-        }, 500).animate({
-          left : "-100"
-        }, 500).animate({
-            left : "100"
-        }, 500).animate({
-          left : "-100"
-        }, 500).animate({
-            left : "100"
-        }, 500);
-    });
-});
+    $('.loadingLine').animate({
+      left : '100'
+      }, 500).animate({
+      left : '-100'
+      }, 500).animate({
+      left : '100'
+      }, 500).animate({
+      left : '-100'
+      }, 500).animate({
+      left : '100'
+      }, 500)
+      })
+})
 
 
 /* REDUNDANT SCRIPT */
@@ -83,10 +83,10 @@ setTimeout(function () {
 
 
   setTimeout(function () {for (var i = 0; i < 2; i++){
-    $('#scrollArrow').animate({
-      bottom: "8"
+    $('.scrollArrow').animate({
+      bottom: '8'
     }, 500).animate({
-      bottom: "5"
+      bottom: '5'
     }, 500);
       }
   }, 6500);
